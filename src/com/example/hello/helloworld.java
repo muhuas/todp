de20b61ce273a78b818c1e7bd51dc442baac7059
时慕华 <shimh@chinatelecom.cn>
2017-06-28 15:17:29 +0800

    modify print

--- src/com/example/hello/helloworld.java
+++ src/com/example/hello/helloworld.java
@@ -6,6 +6,6 @@ package com.example.hello;
 public class helloworld {
     public static void main(String[] args)
     {
-        System.out.println("Hello,World!");
+        System.out.println("Hello,World modify!");
     }
 }
