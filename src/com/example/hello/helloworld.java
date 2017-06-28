e43bdad5ac95dc44a2f2cad856ddc1602b23c52c
时慕华 <shimh@chinatelecom.cn>
2017-06-28 10:27:59 +0800

    test code

---
+++ src/com/example/hello/helloworld.java
@@ -0,0 +1,11 @@
+package com.example.hello;
+
+/**
+ * Created by muhuas on 2017/6/27.
+ */
+public class helloworld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello,World!");
+    }
+}
